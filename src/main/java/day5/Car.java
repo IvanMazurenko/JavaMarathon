f46db39e3c4b79b2004d4b062c4a0e2f02aab0e1
Ivan Mazurenko <ivanmazurenko63@gmail.com>
2021-09-23 20:15:33 +0300

    added class Car in task day5

---
+++ src/main/java/day5/Car.java
@@ -0,0 +1,31 @@
+package day5;
+
+public class Car {
+   private String model;
+   private String color;
+   private int yearOfManufactured;
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYearOfManufactured() {
+        return yearOfManufactured;
+    }
+
+    public void setYearOfManufactured(int yearOfManufactured) {
+        this.yearOfManufactured = yearOfManufactured;
+    }
+}
